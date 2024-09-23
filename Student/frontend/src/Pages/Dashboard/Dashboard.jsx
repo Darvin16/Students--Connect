38dc17270b51aa1ea01985fb7c0f39a4a79fa025
sidharth024gb <124123122+sidharth024gb@users.noreply.github.com>
2024-09-23 16:23:31 +0530

    update

--- Student/frontend/src/Pages/Dashboard/Dashboard.jsx
+++ Student/frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import LibraryRequest from "../LibraryRequest/LibraryRequest";
+import LandingPage from "./LandingPage";
 
 function Dashboard() {
   return (
     <div>
-      <Link to="/dashboard/library/request">Library Rquest</Link>
+      <Link to="/dashboard/library/request">Library Request</Link>
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/" element={<LandingPage/>} />
         <Route path="/library/request" element={<LibraryRequest />} />
       </Routes>
     </div>
---
+++ Student/frontend/src/Pages/Dashboard/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { AppContext } from "../../Context/AppContext";
+
+function LandingPage() {
+  const { userData } = useContext(AppContext);
+
+  return <div>hi {userData.name}</div>;
+}
+
+export default LandingPage;
