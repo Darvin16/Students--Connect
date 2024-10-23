574c19c7e6945067d9041c6af0f06260c290d11c
sidharth024gb <124123122+sidharth024gb@users.noreply.github.com>
2024-10-24 01:11:27 +0530

    update 2

--- Admin/Frontend/src/Pages/Dashboard/Dashboard.jsx
+++ Admin/Frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -48,7 +48,7 @@ function Dashboard() {
 
   return (
     <div className="dashboard-page">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/dashboard">
             Student Connect
